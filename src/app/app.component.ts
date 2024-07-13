8a373a9e17ed35a4ee8cffe6c64fb1116efe7b86
dozipping <dozipping@gmail.com>
2024-07-13 23:49:30 +0700

    ryv3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { News } from './news';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'learning-Ebook';
   news = new Array();
-Expression: any;
-Expression1: any;
-Expression2: any;
 
   constructor() {
-    this.news.push({
-      title: 'Next JS Programming',
-      content: 'Next JS Programming will start on 12 Jan.2024',
-      active: true,
-      important: 1,
-      expire: '2024/4/30',
-    });
-    this.news.push({
-      title: 'Angular Programming',
-      content: 'Angular Programming will start on 13 Jan.2024',
-      active: true,
-      important: 2,
-      expire: '2024/4/30',
-    });
-    this.news.push({
-      title: 'Flutter Programming',
-      content: 'Flutter Programming will start on 14 Jan.2024',
-      active: true,
-      important: 3,
-      expire: '2024/4/30',
-    });
+    this.news.push(
+      new News(
+        'Next JS Programming',
+        'Next JS Programming will start on 12 Jan.2024',
+        true,
+        1,
+        '2024/4/30'
+      )
+    );
+    this.news.push(
+      new News(
+        'Angular Programming',
+        'Angular Programming will start on 13 Jan.2024',
+        false,
+        2,
+        '2024/4/30'
+      )
+    );
+    this.news.push(
+      new News(
+        'Flutter Programming',
+        'Flutter Programming will start on 13 Jan.2024',
+        false,
+        3,
+        '2024/4/30'
+      )
+    );
+  }
+  public getActiveNews():News[]{
+      var news_active = new Array();
+      for(let item of this.news){
+        if(item.active) news_active.push(item);
+      }
+      return news_active;
   }
 }
-
-
